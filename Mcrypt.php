f1c5f590567f47f1bfda22fb16c7428fe0d8c916
Mathis Klooß <mathis@klooss.net>
2017-12-15 09:39:13 +0100

    fixed may issues with undefined elements

--- Mcrypt.php
+++ Mcrypt.php
@@ -2,15 +2,23 @@
 
 // check if mcrypt is loaded
 if (!defined('MCRYPT_MODE_ECB')) {
-    require_once 'phpseclib/Crypt/Base.php';
-    require_once 'phpseclib/Crypt/Rijndael.php';
-    require_once 'phpseclib/Crypt/Twofish.php';
-    require_once 'phpseclib/Crypt/Blowfish.php';
-    require_once 'phpseclib/Crypt/DES.php';
-    require_once 'phpseclib/Crypt/TripleDES.php';
-    require_once 'phpseclib/Crypt/RC2.php';
-    require_once 'phpseclib/Crypt/RC4.php';
-    require_once 'phpseclib/Crypt/Random.php';
-    require_once dirname(__FILE__).'/src/lib/mcrypt.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'Base.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'Rijndael.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'Twofish.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'Blowfish.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'DES.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'TripleDES.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'RC2.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'RC4.php';
+    require_once 'phpseclib'.DS.'Crypt'.DS.'Random.php';
+    require_once __DIR__.DS.'src'.DS.'lib'.DS.'mcrypt.php';
 }
-require_once MAGENTO_ROOT.'/lib/Varien/Crypt/Mcrypt.php';
+$dir = '';
+if (defined('MAGENTO_ROOT'))
+{
+    $dir = MAGENTO_ROOT;
+} else {
+    $dir = BP;
+}
+
+require_once $dir.''.DS.'lib'.DS.'Varien'.DS.'Crypt'.DS.'Mcrypt.php';
